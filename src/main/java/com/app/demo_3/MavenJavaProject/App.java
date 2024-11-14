598cd8132e766a2c0730d5d787c89094fe0256fc
nirajreddy18 <nirajreddy9988@gmail.com>
2024-11-14 15:02:24 +0530

    Maven Java project

---
+++ src/main/java/com/app/demo_3/MavenJavaProject/App.java
@@ -0,0 +1,10 @@
+package com.app.demo_3.MavenJavaProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("G Niraj Reddy - 22BD1A050B");
+    }
+}
